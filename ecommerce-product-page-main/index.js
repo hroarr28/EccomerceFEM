49c91204c8062f9884127bf14986161a88dc4d21
hroarr28 <thcuk1@hotmail.co.uk>
2023-07-20 14:36:58 +0100

    your cart is empty pop up working on mobile

--- ecommerce-product-page-main/index.js
+++ ecommerce-product-page-main/index.js
@@ -3,6 +3,8 @@ const navHamburger = document.querySelector(".hamburger");
 const navClose = document.querySelector(".close-nav");
 const navOverlay = document.querySelector(".nav-overlay");
 
+// mobile menu
+
 navHamburger.addEventListener("click", () => {
   navMenu.classList.add("mobile-navigation-open");
   navOverlay.classList.add("nav-overlay-open");
@@ -13,6 +15,8 @@ navClose.addEventListener("click", () => {
   navOverlay.classList.remove("nav-overlay-open");
 });
 
+//carousel functions
+
 function goLeft() {
   document.querySelector(".image-carousel-container").scrollLeft -= 250;
 }
@@ -20,3 +24,14 @@ function goLeft() {
 function goRight() {
   document.querySelector(".image-carousel-container").scrollLeft += 250;
 }
+
+// cart display functions
+
+const cartDisplay = document.querySelector(".cart-display");
+const cartDisplayLine = document.querySelector(".cart-display-line");
+const cartIcon = document.querySelector(".cart");
+
+cartIcon.addEventListener("click", () => {
+  cartDisplay.classList.toggle("cart-display-open");
+  cartDisplayLine.classList.toggle("cart-display-line-open");
+});
